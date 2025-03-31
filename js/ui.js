95f3d2c4865f57807b29dcd0746cc8fc456c0edf
joelRozenblit <joer0548411989@gmail.com>
2025-03-31 22:46:06 +0300

    css

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,6 @@
 import { COUNTRIES, COUNTRY_MAP, MAIN_DIV, INPUT, DROP_DOWN_MENU, RESULT_DIV } from "./global_variables.js"
 import { addPreviewListener, addBackListener } from "./listeners.js";
-import { getRandomCountries,getNeighboursLinks,getNeighbours,getLanguages,getCurrencies, calculateZoom } from "./helper_functions.js"
+import { getRandomCountries, getNeighboursLinks, getNeighbours, getLanguages, getCurrencies, calculateZoom } from "./helper_functions.js"
 import { handleCountrySelected, handleBackButton, updateActiveCountries } from "./app.js";
 
 
@@ -16,6 +16,7 @@ function updateDropdown(options) {
     options.forEach(country => {
         const li = document.createElement("li");
         li.textContent = country.name.common;
+        li.classList.add("dark");
 
         li.addEventListener("click", () => {
             console.log("li click event");
@@ -64,10 +65,12 @@ function renderPreviewCard(country) {
 
     // הוספת כרטיס למדינה
     MAIN_DIV.insertAdjacentHTML("beforeend", `
-            <div class="card" style="width: 18rem;" id="${sanitizedId}_id">
+            <div class="card gray" style="width: 18rem;" id="${sanitizedId}_id">
+                <div class="img-container" style="padding: 10px;">
                 <img src="${country.flags.png}" class="card-img-top" alt="Flag of ${_name}">
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title">${_name}</h5>
+                    <h5 class="card-title white">${_name}</h5>
                 </div>
             </div>
         `);
@@ -77,26 +80,23 @@ function renderPreviewCard(country) {
 
 
 // כרטיס מדינה מלא
-function printCard(country) {
+async function printCard(country) {
     try {
         console.log(country);
-        
         // קבלת מטבעות, שפות, שכנים ולינקים לשכנים
         const currencies = getCurrencies(country);
         const languages = getLanguages(country);
         const neighbours = getNeighbours(country);
         const borderingCountriesLinks = getNeighboursLinks(neighbours);
-        // חישוב זום מבוסס שטח המדינה
-        const zoom = calculateZoom(country.area);
 
         MAIN_DIV.insertAdjacentHTML("beforeend", `
         <div>
-            <img src="${country.flags.png}" class="" alt="Flag of ${country.name.common}">
+            <img src="${country.flags.png}" alt="Flag of ${country.name.common}">
         </div>
          
         <div>
-            <h2 class="">${country.name.common}</h2>
-            <p class="">
+            <h2>${country.name.common}</h2>
+            <p>
                 <b>Population:</b> ${country.population.toLocaleString()} <br>
                 <b>Region:</b> ${country.region} <br>
                 <b>Capital:</b> ${country.capital ? country.capital[0] : "N/A"} <br>
@@ -105,28 +105,48 @@ function printCard(country) {
                 <b>Bordering Countries:</b> ${borderingCountriesLinks}
             </p>
         </div>
-
-        <div>
-            <iframe 
-                width="100%" 
-                height="400px" 
-                frameborder="0" 
-                scrolling="no" 
-                marginheight="0" 
-                marginwidth="0"
-                src="https://maps.google.com/maps?q=${country.latlng[0]},${country.latlng[1]}&hl=es&z=${zoom}&output=embed">
-            </iframe>
+        
+        <div id="map-container">
+            <span class="loader"></span>
         </div>
     `);
-    
-        handleBackButton();
 
+        // טעינת מפה והצגת ספינר בזמן הטעינה
+        await loadMap(country);
+        // הסרת הספינר רק אם המפה נטענה בהצלחה
+        document.querySelector(".loader").classList.add("hidden");
+        //
+        handleBackButton();
     } catch (error) {
         console.error("Error rendering map or card:", error);
-        document.querySelector("#main_div").innerHTML = `
-            <p>Failed to load map or country details. Please try again later.</p>
+        document.querySelector("#map-container").innerHTML = `
+            <p>Failed to load map or country details.<br> Please try again later.</p>
         `;
     }
+}
+
+
+//
+async function loadMap(country) {
+    // בדיקה אם קיימים קואורדינטות
+    if (!country.latlng || country.latlng.length < 2) {
+        throw new Error("Missing coordinates for country.");
+    }
+
+    // חישוב זום מבוסס שטח המדינה
+    const zoom = calculateZoom(country.area);
+
+    document.querySelector("#map-container").insertAdjacentHTML("beforeend", `
+        <iframe 
+            width="100%" 
+            height="400px" 
+            frameborder="0" 
+            scrolling="no" 
+            marginheight="0" 
+            marginwidth="0"
+            src="https://maps.google.com/maps?q=${country.latlng[0]},${country.latlng[1]}&hl=es&z=${zoom}&output=embed">
+        </iframe>
+    `);
 }
 
 
